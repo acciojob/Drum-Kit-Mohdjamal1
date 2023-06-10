c0c75ae77ad678e0e6aefb3cba59c36ffa37832c
Mohd Jamal <79620177+Mohdjamal1@users.noreply.github.com>
2023-06-10 13:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const url = 'https://pixabay.com/sound-effects/search/clap/';
+const sounds = {
+  'A': 'url',
+  'S': 'url',
+  'D': 'url',
+  'F': 'url',
+  'G': 'url',
+  'H': 'url',
+  'J': 'url',
+  'K': 'url',
+  'L': 'url',
+};
+function playSound(key) {
+  const soundFile = sounds[key];
+  if (soundFile) {
+    const audio = new Audio(soundFile);
+    audio.play();
+  }
+
+document.addEventListener('keydown', function(event) {
+  const key = event.key.toUpperCase();
+  playSound(key);
+});
+}
